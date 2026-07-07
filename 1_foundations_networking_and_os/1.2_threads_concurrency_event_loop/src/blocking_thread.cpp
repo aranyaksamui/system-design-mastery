4aef56b07ee74c423b1801fe70a2dbe366427ed7
Aranyak Samui <aranyaksamuiarya@gmail.com>
2026-07-07 11:47:57 +0000

    Start 1.2 Threads, concurrency and event loop

---
+++ 1_foundations_networking_and_os/1.2_threads_concurrency_event_loop/src/blocking_thread.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <thread>
+#include <chrono>
+#include <vector>
+
+
+
+int main()
+{
+    std::cout << "Starting blocking thread simulation..." << std::endl;
+
+    // A vector of threads
+    std::vector<std::thread> threadPool;
+
+    // Create 3 threads and insert them in threadPool vector
+    for (int i = 0; i < 3; i++)
+    {
+        
+    }
+
+    return 0;
+}
